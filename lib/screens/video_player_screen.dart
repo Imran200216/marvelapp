cbf9dde973566128ed7de588208b7063851c45de
imran <imranbabuji162002@gmail.com>
2024-09-18 15:34:42 +0530

    added the internet checker provider to the video player screen.

--- lib/screens/video_player_screen.dart
+++ lib/screens/video_player_screen.dart
@@ -1,7 +1,9 @@
 import 'package:double_tap_to_exit/double_tap_to_exit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 import 'package:marvelapp/constants/colors.dart';
+import 'package:marvelapp/provider/app_required_providers/internet_checker_provider.dart';
 import 'package:marvelapp/provider/app_required_providers/video_player_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:chewie/chewie.dart';
@@ -20,8 +22,13 @@ class VideoPlayerScreen extends StatelessWidget {
     return ChangeNotifierProvider(
       /// initializing the video player provider
       create: (context) => VideoPlayerProvider(videoUrl),
-      child: Consumer<VideoPlayerProvider>(
-        builder: (context, videoPlayerProvider, child) {
+      child: Consumer2<VideoPlayerProvider, InternetCheckerProvider>(
+        builder: (
+          context,
+          videoPlayerProvider,
+          internetCheckerProvider,
+          child,
+        ) {
           return DoubleTapToExit(
             snackBar: SnackBar(
               backgroundColor: AppColors.timeLineBgColor,
@@ -84,15 +91,53 @@ class VideoPlayerScreen extends StatelessWidget {
                                 : size.width *
                                     0.05, // Adjust space for landscape as well
                           ),
-                          Center(
-                            child: AspectRatio(
-                              aspectRatio: isPortrait ? 16 / 9 : 16 / 10,
-                              child: Chewie(
-                                controller:
-                                    videoPlayerProvider.chewieController,
-                              ),
-                            ),
-                          ),
+                          !internetCheckerProvider.isNetworkConnected
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Lottie.asset(
+                                      'assets/images/animation/robot-animation.json',
+                                      height: size.height * 0.3,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Text(
+                                      "Connection error",
+                                      style: TextStyle(
+                                        fontFamily: "Poppins",
+                                        fontSize: size.width * 0.050,
+                                        fontWeight: FontWeight.w600,
+                                        color: AppColors.secondaryColor,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      margin: const EdgeInsets.only(
+                                        left: 12,
+                                        right: 12,
+                                      ),
+                                      child: Text(
+                                        textAlign: TextAlign.center,
+                                        "It seems you aren't connected to the internet. Try checking your connection or switching between Wi-Fi and cellular data.",
+                                        style: TextStyle(
+                                          fontFamily: "Poppins",
+                                          fontSize: size.width * 0.036,
+                                          fontWeight: FontWeight.w500,
+                                          color: AppColors.subTitleColor,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Center(
+                                  child: AspectRatio(
+                                    aspectRatio: isPortrait ? 16 / 9 : 16 / 10,
+                                    child: Chewie(
+                                      controller:
+                                          videoPlayerProvider.chewieController,
+                                    ),
+                                  ),
+                                ),
                         ],
                       ),
                     );
